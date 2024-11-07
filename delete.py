f26bb252a75a98b5808cd4b880251609a24f0376
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2024-11-08 03:43:26 +0530

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,61 +1,26 @@
 
-def calculate(a,b,c,d,e,f,g,h,i,j,k,l):
-    row1=[int(a),int(b),int(c),int(j)]
-    row2=[int(d),int(e),int(f),int(k)]
-    row3=[int(g),int(h),int(i),int(l)]
-    for i in range(0,len(row1)):
-        row1[i]=row1[i]/row1[0]
-    while not row2[0]== 0:
-        fac=row2[0]/row1[0]
-        for i in range(0,len(row1)):
-            row2[i]=row2[i]-(fac*row1
-                             [i])
-            row2[i]=row2[i]/row2[1]
-        for i in range(0,len(row1)):
-            row2[i]=row2[i]/row2[1]
-            
-    while not row3[0]== 0:
-        fac=row3[0]/row1[0]
-        for i in range(0,len(row1)):
-            row3[i]=row3[i]-(fac*row1
-                             [i])
-        fac2=row3[1]/row2[1]    
+# A very simple Flask Hello World app for you to get started with...
 
-        while not row3[1]==0:
-            for i in range(0,len(row1)):
-                row3[i]=row3[i]-(fac2*row2[i])
+from flask import Flask,request
+from PIL import Image
+from io import BytesIO
+import base64
 
-            for i in range(0,len(row1)):
-                row3[i]=row3[i]/row3[2]
+app = Flask(__name__)
 
-    print(row1)
-    print(row2)
-    print(row3)
+@app.route('/',methods=['GET','POST'])
+def hello_world():
+    data = request.get_json()
+    img1 =data["img1"].encode('UTF-8')
+    img2=data["img2"].encode('UTF-8')
+    def img_save(img,name):
+        image = Image.open(BytesIO(base64.b64decode(img)))
+        image.save("mysite/%s.png"%name, "PNG")
+    img_save(img1,"img1")
+    img_save(img2,"img2")
+
+#    a=df.verify("img1.png","img1.png")
+
+    return str(type(img1))
 
 
-while 0<1:   
-    a=input("(1,1) or input 'd' for demo or 'q' to quit ")
-    if a=="d":
-        calculate(1,1,1,2,3,5,4,0,5,5,8,2)
-            
-    elif a=="q":
-        break
-
-    else:
-        b=input()
-        c=input()
-        d=input()
-        e=input()
-        f=input()
-        g=input()
-        h=input()
-        i=input()
-        j=input()
-        k=input()
-        l=input()
-
-        try:
-            calculate(a,b,c,d,e,f,g,h,i,j,k,l)
-        except:
-            print("the given equations are not solvable")
-            
